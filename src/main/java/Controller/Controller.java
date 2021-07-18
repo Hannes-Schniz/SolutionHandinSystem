8a9ad4f9d40c5cbe58cf2ea8cf8404194e1c2fce
Hannes Schniz <hannes.schniz@gmx.de>
2021-07-18 21:19:36 +0200

    started implementing Controller and Main

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,11 +1,17 @@
 package Controller;
 
-import Human.User;
+import Human.*;
 import Texts.Text;
 
 import java.util.HashMap;
 
 public class Controller {
+
+    private static String USER_DOZENT = "Dozent";
+
+    private static String USER_TUTOR = "Tutor";
+
+    private static String USER_STUDENT = "Student";
 
     private HashMap<String, User> userList;
 
@@ -13,9 +19,33 @@ public class Controller {
 
     private UI userInterface;
 
-    public Controller(HashMap<String, User> userList, HashMap<String, Text> textList, UI userInterface) {
-        this.userList = userList;
-        this.textList = textList;
+    public Controller( UI userInterface) {
+        this.userList = new HashMap<String, User>();
+        this.textList = new HashMap<String, Text>();
         this.userInterface = userInterface;
+    }
+
+    public HashMap<String, User> getUserList() {
+        return userList;
+    }
+
+    public HashMap<String, Text> getTextList() {
+        return textList;
+    }
+
+    public UI getUserInterface() {
+        return userInterface;
+    }
+
+    public void addUser(User newUser){
+        if (newUser.getClass().equals(Dozent.class)){
+            userList.put(USER_DOZENT , newUser);
+        }
+        else if (newUser.getClass().equals(Tutor.class)){
+            userList.put(USER_TUTOR , newUser);
+        }
+        else if (newUser.getClass().equals(Student.class)){
+            userList.put(USER_STUDENT , newUser);
+        }
     }
 }
---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import Controller.Controller;
+
+public class Main {
+
+    private static Controller cntrl;
+
+    public static void main(String[] args) {
+
+    }
+}

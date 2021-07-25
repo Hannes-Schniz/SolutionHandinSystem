f7095d61ddfc8d24ab3c5ee35d1560af5b3e1ec1
Hannes Schniz <hannes.schniz@gmx.de>
2021-07-25 16:55:07 +0200

    implemented Datacollection

---
+++ src/main/java/Corrections/Datacollection.java
@@ -0,0 +1,62 @@
+package Corrections;
+
+public class Datacollection {
+
+    private static int MINUS_ONE = -1;
+
+    private static String EMPTY_STRING = "";
+
+    private String biggestString;
+
+    private int length;
+
+    private double percent;
+
+    public Datacollection() {
+
+        this.biggestString = EMPTY_STRING;
+        this.length = MINUS_ONE;
+        this.percent = MINUS_ONE;
+    }
+
+    public static int getMinusOne() {
+        return MINUS_ONE;
+    }
+
+    public static void setMinusOne(int minusOne) {
+        MINUS_ONE = minusOne;
+    }
+
+    public static String getEmptyString() {
+        return EMPTY_STRING;
+    }
+
+    public static void setEmptyString(String emptyString) {
+        EMPTY_STRING = emptyString;
+    }
+
+    public String getBiggestString() {
+        return biggestString;
+    }
+
+    public void setBiggestString(String biggestString) {
+        this.biggestString = biggestString;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(double percent) {
+        this.percent = percent;
+    }
+
+}

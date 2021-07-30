e2c03216b31845f3c4efebc75dc7d6203f8425c5
Hannes Schniz <hannes.schniz@gmx.de>
2021-07-30 17:30:24 +0200

    implented parser class

---
+++ src/main/java/Interface/Parser.java
@@ -0,0 +1,72 @@
+package Interface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    private static int ONE = 1;
+
+    private static int ZERO = 0;
+
+    public Parser() {
+
+    }
+
+    public String[] parseInput(String input) throws IllegalArgumentException{
+
+        String parted = "";
+        int spaces = ZERO;
+        String[] returnArray = new String[ONE];
+
+        if (!input.matches("[a-zA-Z0-9_]")){
+            throw new IllegalArgumentException("wrong Input");
+        }
+
+        for (int i = ZERO; i < input.length(); i++) {
+            if (input.charAt(i) == ' '){
+                if (spaces == ZERO){
+                    returnArray[ZERO] = parted;
+                }
+                else {
+                    extendArray(returnArray);
+                    returnArray[returnArray.length - ONE] = parted;
+                }
+                spaces++;
+            }
+            else {
+                parted += input.charAt(i);
+            }
+        }
+
+        if (spaces < 2){
+            throw new IllegalArgumentException("input too short");
+        }
+
+        return returnArray;
+    }
+
+    private String[] extendArray(String[] input){
+
+        String[] workingArray = input;
+        input = new String[input.length + ONE];
+
+        for (int i = 0; i < workingArray.length; i++) {
+            input[i] = workingArray[i];
+        }
+
+        return input;
+    }
+
+    public int parseNumber(String input) throws IllegalArgumentException{
+
+        if (!input.matches("[0-9]")){
+            throw new IllegalArgumentException("wrong input");
+        }
+
+        int returnInt = Integer.parseInt(input);
+
+        return returnInt;
+
+    }
+}

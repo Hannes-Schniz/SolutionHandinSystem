7399d4de6ef58c0f14fa47cb59b921997303504f
Hannes Schniz <hannes.schniz@gmx.de>
2021-07-30 16:37:12 +0200

    implemented Terminal facade to read capsule the console

---
+++ src/main/java/Interface/Terminal.java
@@ -0,0 +1,28 @@
+package Interface;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Terminal {
+
+    BufferedReader reader;
+
+    public Terminal() {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void println(String line){
+        System.out.println(line);
+    }
+
+    public void printlines(String[] lines){
+        for (int i = 0; i < lines.length; i++) {
+            System.out.println(lines[i]);
+        }
+    }
+
+    public String readln() throws IOException {
+        return reader.readLine();
+    }
+}
